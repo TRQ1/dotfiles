5c9a468d14a02993d1ec0d044c7483a60483dc77
trq1 <trq1.lee@gmail.com>
2020-01-29 21:55:20 +0900

    git command를 정리 하라
    * git commit 을 효율적으로 하게 --allow-empty 옵션을 확인 했다.
    * git commit log를 확인 하는 옵션을 정리했다.
    * git commit 내역 돌리는 명령어를 정리 했다.
    * git commit list를 확인 하는 명령어를 stackoverflow를 참고하여 정리 하였다.

---
+++ bin/git.bash
@@ -0,0 +1,20 @@
+# 내용 없이 git commit 
+git commit --allow-empty
+
+# 커밋간의 다른 부분 확인
+git diff <branchA> <branchB>
+
+# commit 내역 돌리기
+git reset --soft HEAD <commit>
+
+# commit log 조회
+git log -p <number>
+
+# commit 리스트 확인 from https://stackoverflow.com/questions/1838873/visualizing-branch-topology-in-git
+git log log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'
+
+# 해당 파일 commit 리스트 확인
+git log --follow <file>
+
+# commit 정보 확인
+git show 
